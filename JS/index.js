ff3050613ec9b21943462bfd73981d721aad889c
Pedro Fernandes <pedrofdnn@gmail.com>
2022-11-21 17:30:35 -0300

    Projeto concluido

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,21 @@
-// campo onde vai aparecer o nome,ID,Imagen do pokemon
+// variaveis nome,ID,Imagen do pokemon
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_id');
 const pokemonImage = document.querySelector('.pokemon_image');
 
-// evento de busca no inpunt
+// variaveis evento de busca no inpunt e botão de proximo
 const form = document.querySelector('.form');
 const input = document.querySelector('.inpunt_search');
+const buttonPrev = document.querySelector('.bnt_prev');
+const buttonNext = document.querySelector('.bnt_next');
+
+// variavel de pagina inicial com o pokemon renderizado.
+let searchPokemon = 1
+
 
 // função que realiza a busca na API
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     //quando a informacao nao encontrar nada ele nao irar retornar nada.
     if (APIResponse.status === 200) {
@@ -24,13 +30,15 @@ const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
     if (data) {
-        pokemonName.innerHTML = data.name;
+        pokemonImage.style.display = 'block'
+        pokemonName.innerHTML = data.name.toLowerCase();
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']
         ['black-white']['animated']['front_default'];
         input.value = ''
-        input.value = ''
+        searchPokemon = data.id
     } else {
+        pokemonImage.style.display = 'none'
         pokemonName.innerHTML = 'Not Foud :c'
         pokemonNumber.innerHTML = ''
     }
@@ -40,5 +48,21 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     renderPokemon(input.value)
+})
+
+//Funçao do botão next
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
 
 })
+
+//Funçao do botão prev
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1
+        renderPokemon(searchPokemon)
+    }
+})
+
+renderPokemon(searchPokemon)
